a79b35b3b31bdde2d6309a5067e806c22181d9e7
Shreya Sharma <vshlvani1234@gmail.com>
2025-03-05 15:03:59 +0530

    added basic CRUD functionalities

---
+++ index.js
@@ -0,0 +1,124 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
+const { v4: uuid } = require("uuid");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+let comments = [
+  {
+    id: uuid(),
+    username: "Todd",
+    comment: "Todd's comment",
+    replies: [],
+  },
+  {
+    id: uuid(),
+    username: "Jack",
+    comment: "Jack's comment",
+    replies: [],
+  },
+  {
+    id: uuid(),
+    username: "John",
+    comment: "John's comment",
+    replies: [],
+  },
+  {
+    id: uuid(),
+    username: "Hannah",
+    comment: "Hannah's comment",
+    replies: [],
+  },
+  {
+    id: uuid(),
+    username: "Timmie",
+    comment: "Timmie's comment",
+    replies: [],
+  },
+  {
+    id: uuid(),
+    username: "Tyla",
+    comment: "Tyla's comment",
+    replies: [],
+  },
+  {
+    id: uuid(),
+    username: "Fred",
+    comment: "Fred's comment",
+    replies: [],
+  },
+];
+
+app.get("/comments", (req, res) => {
+  res.render("comments/index", { comments });
+});
+
+//
+app.get("/comments/new", (req, res) => {
+  res.render("comments/new");
+});
+
+app.post("/comments", (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({ username, comment, id: uuid() });
+  res.redirect("/comments");
+});
+
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+
+  res.render("comments/show", { comment });
+});
+
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/edit", { comment });
+});
+
+app.patch("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const newCommentText = req.body.comment;
+  const foundComment = comments.find((c) => c.id === id);
+  foundComment.comment = newCommentText;
+  res.redirect("/comments");
+});
+
+app.delete("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  comments = comments.filter((c) => c.id !== id);
+  res.redirect("/comments");
+});
+
+//
+
+app.post("/tacos", (req, res) => {
+  console.log(req.body);
+  res.send("POST /tacos response");
+});
+
+app.get("/tacos", (req, res) => {
+  res.send("GET /tacos response");
+});
+
+app.listen(3000, () => {
+  console.log("ON PORT 3000!");
+});
+
+/*
+
+-- PATTERN-- (not standard)
+GET /comments - list all comments
+POST /comments - create a new comment
+GET /comments/:id - get one comment using ID
+PATCH /comments/:id - update one comment
+DELETE /comments/:id - destroy one comment 
+
+*/
